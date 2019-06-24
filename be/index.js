76b6f7ad01e82d18c020a3fd473da413751d61d5
Tamás Bordák <bordak.tamas@autsoft.hu>
2019-06-24 15:18:56 +0200

    list scrape

--- be/index.js
+++ be/index.js
@@ -1,29 +1,66 @@
-const http = require('http')
-const port = 3000
-
-const requestHandler = (request, response) => {
-    const end = request.url.split('/');
-    const name = end[end.length-1].split('.')[0];
-    console.log(name);
-    if(name == 'islogin') {
-        response.end('false')
-    } else if (name == 'login') {
-        response.end('"good"')
-    } else if (name == 'getBike') {
-        response.end('should get a bike')
-    } else if (name == 'logout') {
-        response.end('')
-    } else if (name == 'endings') {
-        response.end('list')
-    } 
+function createState(inProgress) {
+    return { inProgress }
 }
 
-const server = http.createServer(requestHandler)
+function getLatest() {
+    return [createState(true), createState(false), createState(false), createState(false), createState(false)]
+}
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
 
-  console.log(`server is listening on ${port}`)
-})
+
+function getConcurrentCount(states) {
+    return states.filter(x => x.inProgress).length
+}
+
+function scrape() {
+    console.log('sc');
+    var url = require('url');
+    var http = require('https');
+    var endpoint = 'https://bubi.nextbike.net/iframe/';
+    var opts = url.parse(endpoint);
+    opts.method = 'GET';
+    opts.host = "bubi.nextbike.net"
+    opts.path = "/iframe/"
+    opts.headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'hu-HU,hu;q=0.9,en-US;q=0.8,en;q=0.7,nl;q=0.6',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Cookie': 'PHPSESSID=d089e777741ce0eb9b87d70f9dbb5fec',
+        'Host': 'bubi.nextbike.net',
+        'Pragma': 'no-cache',
+        'Upgrade-Insecure-Requests': '1'
+    }
+    var options = { 
+        hostname: 'example.com',
+        path: '/somePath.php',
+        method: 'GET',
+        headers: {'Cookie': 'myCookie=myvalue'}
+    };
+    var results = ''; 
+    var req = http.request(opts, function(res) {
+        res.on('data', function (chunk) {
+            results = results + chunk;
+            
+        }); 
+        res.on('end', function () {
+            console.log(results);
+        }); 
+    });    
+    req.on('error', function(e) {
+    });    
+    req.end();
+
+    // http.request(options, function (res) {
+    //     console.log(res.statusCode);
+    //     res.on('data', function (chunk) {
+    //         results = results + chunk;
+    //     }); 
+    //     res.on('end', function () {
+    //         console.log('end');
+    //     }); 
+    // });
+}
+scrape();
+// console.log(getConcurrentCount(getLatest()))
---
+++ be/mock.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const port = 3000
+const url = require('url')
+
+const requestHandler = (req, response) => {
+    const end = req.url.split('/');
+    const name = end[end.length-1].split('.')[0];
+    var url_parts = url.parse(req.url,true);
+    console.log(name);
+    if(name == 'islogin') {
+        response.end('false')
+    } else if (name == 'login') {
+        if(url_parts.query.username && url_parts.query.password) {
+            login(response, url_parts.query.username, url_parts.query.password);
+        } else {
+            response.end('"bad login"')
+        }
+    } else if (name == 'getBike') {
+        getBike(response, url_parts.query.code);
+    } else if (name == 'logout') {
+        response.end('')
+    } else if (name == 'endings') {
+        response.end('list')
+    } 
+}
+
+function login(response, user, pass) {
+    if(user == 'tomi' && pass == '123') {
+        response.end('"good"')
+    } else {
+        response.end('"bad login"')
+    }
+}
+
+function getBike(response, code) {
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
