f64b35bb3cfe9c47cda45836e393e69fbd0f593b
Tamás Bordák <bordak.tamas@autsoft.hu>
2019-07-05 02:20:49 +0200

    flash setinter

--- src/app/new-domain/new-domain.component.ts
+++ src/app/new-domain/new-domain.component.ts
@@ -23,9 +23,9 @@ export class NewDomainComponent implements OnInit {
   ngOnInit() {
     console.log('new domain component init');
     this.reading = this.login;
-    setTimeout(() => {
+    setInterval(() => {
       this.torch = true;
-    }, 1);
+    }, 100);
   }
 
   onChange(e) {
